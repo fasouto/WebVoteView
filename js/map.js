255b8fdb2d2c2be3961ee8473355207e7398fed8
Fabio Souto <fsoutomoure@gmail.com>
2013-12-16 22:36:19 +0100

    First pass of JSLint

--- js/map.js
+++ js/map.js
@@ -1,3 +1,4 @@
+/*jslint white: true */
 function webVoteMap(element, data, options) {
 
   // Configurable variables
@@ -7,27 +8,25 @@ function webVoteMap(element, data, options) {
     transitionDuration: 750,
     zoomLevel: 10,
     staticUrl: "http://leela.sscnet.ucla.edu/voteview_static/" // URL where the static content is stored(images...)
-  } 
+  };
 
   // Compose Settings Object
   var settings = $.extend(defaults_map, options);
 
-  var centered;
   var aspect = settings.width / settings.height;
 
   // Get some D3 DOM objects and create others
   var projection = d3.geo.albersUsa();
-  var path = d3.geo.path()
-      .projection(projection);
+  var path = d3.geo.path().projection(projection);
   var svgmap, g, sb;
-  var tooltip = d3.select("body").append("div") .attr("class", "wvv-tooltip");
+  var tooltip = d3.select("body").append("div").attr("class", "wvv-tooltip");
 
   // Map vote choices
   var voteChoices = {
-    "1":'Yea', "2":"Yea", "3":"Yea", 
-    "4":"Nay", "5":"Nay", "6":'Nay', 
-    "7":"Abs", "8":"Abs", "9":"Abs"
-  }
+    "1": 'Yea', "2": "Yea", "3": "Yea", 
+    "4": "Nay", "5": "Nay", "6": "Nay", 
+    "7": "Abs", "8": "Abs", "9": "Abs"
+  };
 
   // Dictionaries of members
   var congressMembers = {};
@@ -53,8 +52,7 @@ function webVoteMap(element, data, options) {
     "YeaIndependent": "#FFDD00",
     "NayIndependent": "#FFDDAA",
     "AbsIndependent": "#DDD"
-  }
-
+  };
 
 
   // Initialise
@@ -62,24 +60,24 @@ function webVoteMap(element, data, options) {
 
   // Set the member dictionary
   function mapCongressMembersVotes(members, votation) {
-    members.forEach( function(d) {
-        if (d.id in votation.votes) { // If the member had voted
-           var dist = parseInt(d.districtCode) > 70 ? 0 : parseInt(d.districtCode);  // 00 = At large
-           d["vote"] = votation.votes[d.id]; // Add the vote to the member
-           if (congressMembers[sprintf("%s%02d", d.stateAbbr, dist)] != undefined) {
-            congressMembers[sprintf("%s%02d", d.stateAbbr, dist)].push(d);
-           }
-           else {
-            congressMembers[sprintf("%s%02d", d.stateAbbr, dist)] = [d];
-           }
+    members.forEach(function(d) {
+      if (d.id in votation.votes) { // If the member had voted
+        var dist = parseInt(d.districtCode, 10) > 70 ? 0 : parseInt(d.districtCode, 10);  // 00 = At large
+        d.vote = votation.votes[d.id]; // Add the vote to the member
+        if (congressMembers[sprintf("%s%02d", d.stateAbbr, dist)] !== undefined) {
+          congressMembers[sprintf("%s%02d", d.stateAbbr, dist)].push(d);
         }
-     })
+        else {
+          congressMembers[sprintf("%s%02d", d.stateAbbr, dist)] = [d];
+        }
+      }
+     });
   }
 
   function mapSenateMembersVotes(members, votation) {
-    members.forEach( function(d) {
+    members.forEach(function(d) {
         if (d.id in votation.votes) { // If the member had voted
-           d["vote"] = votation.votes[d.id]; // Add the vote to the member
+           d.vote = votation.votes[d.id]; // Add the vote to the member
            if (senateMembers[d.stateAbbr] != undefined) {
             senateMembers[d.stateAbbr].push(d);
            }
@@ -87,19 +85,20 @@ function webVoteMap(element, data, options) {
             senateMembers[d.stateAbbr] = [d];
            }
         }
-     })
+     });
   }
 
   // Render the tooltip
   function tooltipHTML(members, id) {
-    var tooltipContent="";
+    var tooltipContent = "";
+    var index;
     var allMembers = members[id];
     var atLargeMembers = members[members[id][0].stateAbbr + "00"];
     if (atLargeMembers !== undefined) {
       allMembers = $.unique(members[id].concat(atLargeMembers));
     }
-    for (var index in allMembers) {
-      tooltipContent += sprintf("<img src=\"%simg/img%06ds.png\" onerror=\"null;this.src='img/no_image.png';\"/><p><strong>%s</strong></p><p>%s %s</p><p>Vote:%s</p>", settings.staticUrl, parseInt(allMembers[index]['icpsr']), allMembers[index]['fname'], allMembers[index]['partyname'], allMembers[index]['cqlabel'], allMembers[index]['vote']);
+    for (index in allMembers) {
+      tooltipContent += sprintf("<img src=\"%simg/img%06ds.png\" onerror=\"null;this.src='img/no_image.png';\"/><p><strong>%s</strong></p><p>%s %s</p><p>Vote:%s</p>", settings.staticUrl, parseInt(allMembers[index].icpsr), allMembers[index].fname, allMembers[index].partyname, allMembers[index].cqlabel, allMembers[index].vote);
     }
     return tooltipContent;
   }
@@ -125,7 +124,7 @@ function webVoteMap(element, data, options) {
          }
          // return 'district';
          return "beige";
-      })
+      });
   }
 
   function shadeStates(votation) {
@@ -146,7 +145,7 @@ function webVoteMap(element, data, options) {
             return "red";
           }
         return "beige";
-      })
+      });
   }
 
   // Zoom on click
@@ -186,8 +185,8 @@ function webVoteMap(element, data, options) {
 
   // Blend an array of colors
   function blendColors(colors) {
-    var r = 0, g = 0 , b = 0;
-    for (var i=0; i<colors.length; i++) {
+    var r = 0, g = 0 , b = 0, i, rgbColor;
+    for (i=0; i<colors.length; i++) {
       rgbColor = d3.rgb(colors[i]);
       r = r + rgbColor.r;
       g = g + rgbColor.g;
@@ -204,10 +203,11 @@ function webVoteMap(element, data, options) {
       var targetWidth = svgmap.node().parentNode.offsetWidth;
       svgmap.attr("width", targetWidth);
       svgmap.attr("height", targetWidth / aspect);
-  };
+  }
 
   // Main function to draw the map after data load
   function chart(element, data) {
+    var centered;
     svgmap = d3.select(element)
       .attr("xmlns", "http://www.w3.org/2000/svg")
       .attr("width", settings.width)
@@ -220,7 +220,7 @@ function webVoteMap(element, data, options) {
     mapSenateMembersVotes(data.members, data.votation);
     mapCongressMembersVotes(data.members, data.votation);
 
-    if (data.votation.chamber == "Senate") {
+    if (data.votation.chamber === "Senate") {
       // Color the states
       sb.selectAll(".state")
         .data(topojson.feature(data.states, data.states.objects.states).features).enter().append("path")
@@ -240,7 +240,7 @@ function webVoteMap(element, data, options) {
 
       shadeStates(data.votation);
     }
-    else if (data.votation.chamber == "House") {
+    else if (data.votation.chamber === "House") {
       g = svgmap.append("g").attr("id","map-group");
       // Color the districts
       g.selectAll(".district")
@@ -263,7 +263,7 @@ function webVoteMap(element, data, options) {
       // // Add state boundaries
       sb.append("path")
         .datum(topojson.mesh(data.states, data.states.objects.states, 
-             function(a, b) { return a.id != "AK" & a.id != "HI"}))
+             function(a, b) { return a.id !== "AK" && a.id !== "HI"; }))
         .attr("d", path)
         .attr("class", "state-boundary");
 
@@ -271,6 +271,6 @@ function webVoteMap(element, data, options) {
     }
 
     resizeMap();
-  };
+  }
 }
 
