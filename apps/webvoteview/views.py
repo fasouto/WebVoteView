3257e61a624b585b036ebdb5a108746bef6d1866
Fabio Souto <fsoutomoure@gmail.com>
2014-09-01 20:18:10 +0200

    Improved export to excel

--- apps/webvoteview/views.py
+++ apps/webvoteview/views.py
@@ -23,7 +23,11 @@ def download_excel(request):
     """
     Download an excel with the rollcall
     """
-    rollcall_ids = request.GET.getlist('rollcalls')
+    rollcall_params = request.GET.getlist('rollcall')
+
+    rollcalls = {}
+    for i,rollcall in enumerate(rollcall_params):
+        rollcalls["V" + str(i+1)] = rollcall
 
     # Create the excel workbook and sheets and define the basic styles
     book = xlwt.Workbook(encoding='utf8')
@@ -35,7 +39,8 @@ def download_excel(request):
 
     # Let's write the headers
     vote_matrix_cols = ['ICSPR', 'State', 'State Abbr','District', 'CQ label', 'Name', 'Full name', 'Party code', 'Party name']
-    vote_matrix_cols.append(["V" + str(i+1) for i in range(len(rollcall_ids))])
+    vote_matrix_cols.extend(rollcalls.keys())
+
     rollcall_desc_cols = ['Vote', 'Chamber', 'Congress', 'Date', 'Rollnumber', 'Description']
     for col, title in enumerate(vote_matrix_cols):
         sheet_vote.write(0, col, title, style=default_style)
@@ -43,38 +48,55 @@ def download_excel(request):
         sheet_rollcall.write(0, col, title, style=default_style)
 
 
-    for rollcall_index, rollcall_id in enumerate(rollcall_ids, start=1):
-        rollcall = db.voteview_rollcalls.find({'id': rollcall_id})[0]
+    members_matrix = {}
 
-        # Write the vote matrix sheet
-        for row_index, member_id in enumerate(rollcall['votes'], start=1):
-            member = db.voteview_members.find_one({'id':member_id})
-            sheet_vote.write(row_index, 0, member['icpsr'], style=default_style)
-            sheet_vote.write(row_index, 1, member['state'], style=default_style)
-            sheet_vote.write(row_index, 2, member['stateAbbr'], style=default_style)
-            sheet_vote.write(row_index, 3, member['districtCode'], style=default_style)
-            sheet_vote.write(row_index, 4, member['cqlabel'], style=default_style)
-            sheet_vote.write(row_index, 5, member['name'], style=default_style)
-            sheet_vote.write(row_index, 6, member['fname'], style=default_style)
-            sheet_vote.write(row_index, 7, member['party'], style=default_style)
-            sheet_vote.write(row_index, 8, member['partyname'], style=default_style)
-            sheet_vote.write(row_index, 9, rollcall['votes'][member_id], style=default_style)
+    for rollcall_index, rollcall_id in enumerate(rollcalls, start=1):
+        rollcall = db.voteview_rollcalls.find({'id': rollcalls[rollcall_id]})[0]
 
         # Write the rollcall descriptions sheet
-        sheet_rollcall.write(rollcall_index, 0, "V1", style=default_style)
+        sheet_rollcall.write(rollcall_index, 0, rollcall_id, style=default_style)
         sheet_rollcall.write(rollcall_index, 1, rollcall['chamber'], style=default_style)
         sheet_rollcall.write(rollcall_index, 2, rollcall['session'], style=default_style)
         sheet_rollcall.write(rollcall_index, 3, rollcall['date'], style=default_style)
         sheet_rollcall.write(rollcall_index, 4, rollcall['rollnumber'], style=default_style)
         sheet_rollcall.write(rollcall_index, 5, rollcall['description'], style=default_style)
 
+        # Populate the members matrix
+        for member_id in rollcall['votes']:
+            member = db.voteview_members.find_one({'id':member_id})
+            if member['icpsr'] not in members_matrix.keys():
+                members_matrix[member['icpsr']] = {}
+            members_matrix[member['icpsr']]['ICSPR'] = member['icpsr']
+            members_matrix[member['icpsr']]['State'] = member['state']
+            members_matrix[member['icpsr']]['State Abbr'] = member['stateAbbr']
+            members_matrix[member['icpsr']]['District'] = member['districtCode']
+            members_matrix[member['icpsr']]['CQ label'] = member['cqlabel']
+            members_matrix[member['icpsr']]['Name'] = member['name']
+            members_matrix[member['icpsr']]['Full name'] = member['fname']
+            members_matrix[member['icpsr']]['Party code'] = member['party']
+            members_matrix[member['icpsr']]['Party name'] = member['partyname']
+            members_matrix[member['icpsr']][rollcall_id] = rollcall['votes'][member_id]
+
+    # Write the vote matrix sheet
+    for row_index, member in enumerate(members_matrix, start=1):
+        # print members_matrix[member]
+        sheet_vote.write(row_index, 0, members_matrix[member]['ICSPR'], style=default_style)
+        sheet_vote.write(row_index, 1, members_matrix[member]['State'], style=default_style)
+        sheet_vote.write(row_index, 2, members_matrix[member]['State Abbr'], style=default_style)
+        sheet_vote.write(row_index, 3, members_matrix[member]['District'], style=default_style)
+        sheet_vote.write(row_index, 4, members_matrix[member]['CQ label'], style=default_style)
+        sheet_vote.write(row_index, 5, members_matrix[member]['Name'], style=default_style)
+        sheet_vote.write(row_index, 6, members_matrix[member]['Full name'], style=default_style)
+        sheet_vote.write(row_index, 7, members_matrix[member]['Party code'], style=default_style)
+        sheet_vote.write(row_index, 8, members_matrix[member]['Party name'], style=default_style)
+        for idx_vote, rollcall_key in enumerate(rollcalls.keys(), start=9):
+            sheet_vote.write(row_index, idx_vote, members_matrix[member][rollcall_key] or 0, style=default_style)
 
     # Download the excel file
     response = HttpResponse(mimetype='application/vnd.ms-excel')
     response['Content-Disposition'] = 'attachment; filename=rollcalls.xls'
     book.save(response)
     return response
-    # return HttpResponse('asd')
 
 
 def ajax_faceted_search(request):
